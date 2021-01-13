9efc9680682dcc9bd9d2ed4f9acba7a257616ba4
Visckmart <5529302+Visckmart@users.noreply.github.com>
2021-01-13 05:21:31 -0300

    Texto desenhado nas arestas.

--- Structure/UndirectedEdge.js
+++ Structure/UndirectedEdge.js
@@ -1,5 +1,5 @@
 import Edge from "./Edge.js";
-import {ctx} from "../Drawing/General.js";
+import {canvas, ctx} from "../Drawing/General.js";
 
 export class UndirectedEdge extends Edge {
     constructor(label) {
@@ -20,5 +20,44 @@ export class UndirectedEdge extends Edge {
         ctx.stroke();
 
         ctx.restore()
+
+
+        ctx.save();
+
+        this._drawText({ x: xStart, y: yStart },
+                       { x: xEnd,   y: yEnd   })
     }
+
+    _drawText({ x: xStart, y: yStart },
+              { x: xEnd,   y: yEnd   }) {
+        
+        // yEnd -= vertOffset
+        // yStart -= vertOffset
+        // Calcula o meio da linha
+        let midX = (xEnd - xStart)/2
+        let midY = (yEnd - yStart)/2
+
+        ctx.translate(xStart + midX, yStart + midY);
+
+        // Calcula o ângulo do texto
+        let angle = Math.atan2(midY, midX)
+        // Não permite que o texto fique de cabeça para baixo
+        if (Math.abs(angle) > Math.PI/2) {
+            angle = Math.atan2(-midY, -midX)
+        }
+        ctx.rotate(angle);
+        
+        // Deslocamento vertical faz com que o texto não seja desenhado
+        // por cima da linha
+        let vertOffset = 15
+        ctx.translate(0, -vertOffset);
+
+        ctx.font = "bold 15pt Arial";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.fillText(this.label, 0, 0);
+        
+        ctx.restore();
+    }
+
 }
