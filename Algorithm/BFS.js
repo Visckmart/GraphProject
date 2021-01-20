98e37f64772faa9cfe6bc602a1639b7eb78ca486
Thlamz <thlamenza@hotmail.com>
2021-01-20 17:04:06 -0300

    Implementando BFS

---
+++ Algorithm/BFS.js
@@ -0,0 +1,47 @@
+import AlgorithmController from "../Drawing/AlgorithmControls/AlgorithmController.js";
+import {NodeHighlightType} from "../Structure/Node.js";
+
+
+export default function BFS(controller, startNode)
+{
+    let graph = controller.initialGraph
+
+    let queue = [startNode]
+
+    let lastNode = null
+    let currentNode = null
+    while(queue.length > 0){
+        if(currentNode)
+        {
+            lastNode = currentNode
+            lastNode.removeHighlight(NodeHighlightType.ALGORITHM_FOCUS)
+        }
+
+        currentNode = queue.shift()
+        if(currentNode)
+        {
+            currentNode.addHighlight(NodeHighlightType.ALGORITHM_FOCUS)
+            currentNode.visited = true
+        } else {
+            break
+        }
+
+        controller.addStep(graph)
+
+        let anyEdges = false
+        for(let [edge, node] of graph.edgesFrom(currentNode))
+        {
+            if(!node.visited)
+            {
+                console.log(node)
+                queue.push(node)
+                edge.addHighlight(NodeHighlightType.ALGORITHM_FOCUS)
+                anyEdges = true
+            }
+        }
+        if(anyEdges)
+        {
+            controller.addStep(graph)
+        }
+    }
+}
