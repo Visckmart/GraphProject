7c5443ebc62730f9c40251e837f66bb922ca2f4f
Thlamz <thlamenza@hotmail.com>
2021-02-01 10:43:22 -0300

    Implementando requisito de create_edge

--- Drawing/AlgorithmControls/AlgorithmRequirements.js
+++ Drawing/AlgorithmControls/AlgorithmRequirements.js
@@ -60,12 +60,72 @@ export class Requirement {
                             resolve(newNode)
                         }
                     }
+                    this.inputHandler.canvas.addEventListener("mouseup", handler)
                     break
                 }
                 // Requisito de criação de aresta
-                // TODO: Implementar
                 case RequirementType.CREATE_EDGE:
-                    console.warn("REQUISITO NÃO IMPLEMENTADO")
+                    this.inputHandler.changeCursorStyle("pointer")
+                    // Primeira node da aresta
+                    let firstNode
+                    // Segunda node da aresta
+                    let secondNode
+
+                    // Handler de mousedown usado para capturar a primeira node da aresta
+                    let mouseDownHandler = (event) => {
+                        let mousePos = this.inputHandler.getMousePos(this.inputHandler.canvas, event)
+                        let nodesAtClick = this.inputHandler.graphView.getNodeIndexAt(mousePos)
+                        if(nodesAtClick.length > 0)
+                        {
+                            firstNode = nodesAtClick[0]
+                            this.inputHandler.changeCursorStyle("grabbing")
+
+                            // TODO: Não gosto dessas linhas
+                            // Desenhando aresta temporária
+                            this.inputHandler.graphView.interactionHandler.mouse.clickPosition = mousePos
+                            this.inputHandler.graphView.interactionHandler.mouse.shouldDrawTemporaryEdge = true
+
+                            // Remove o evento para evitar repetição
+                            this.inputHandler.canvas.removeEventListener("mousedown", mouseDownHandler)
+                            this.inputHandler.canvas.addEventListener("mouseup", mouseUpHandler)
+                            this.inputHandler.canvas.addEventListener("mousemove", mouseMoveHandler)
+                        }
+                    }
+                    this.inputHandler.canvas.addEventListener("mousedown", mouseDownHandler)
+
+                    // Handler de movimentação do mouse para redesenhar o grafo, atualizando a aresta temporária
+                    let mouseMoveHandler = (event) => {
+                        this.inputHandler.graphView.interactionHandler.mouse.currentMousePos =
+                            this.inputHandler.getMousePos(this.inputHandler.canvas, event)
+                        this.inputHandler.graphView.redrawGraph()
+                    }
+
+                    // Handler de mouseup para seleção do segundo node da aresta
+                    let mouseUpHandler = (event) => {
+                        this.inputHandler.graphView.interactionHandler.mouse.shouldDrawTemporaryEdge = false
+                        this.inputHandler.canvas.removeEventListener("mouseup", mouseUpHandler)
+                        this.inputHandler.canvas.removeEventListener("mousemove", mouseMoveHandler)
+
+                        let mousePos = this.inputHandler.getMousePos(this.inputHandler.canvas, event)
+                        let nodesAtClick = this.inputHandler.graphView.getNodeIndexAt(mousePos)
+                        if(nodesAtClick.length > 0
+                            && nodesAtClick[0] !== firstNode
+                            && !this.inputHandler.graphView.structure.checkEdgeBetween(nodesAtClick[0], firstNode))
+                        {
+                            secondNode = nodesAtClick[0]
+                            this.inputHandler.graphView.insertEdgeBetween(firstNode, secondNode)
+                            this.inputHandler.graphView.redrawGraph()
+
+                            this.inputHandler.changeCursorStyle(null)
+                            this.callback([firstNode, secondNode])
+                            resolve([firstNode, secondNode])
+                        } else {
+                            firstNode = null
+                            secondNode = null
+                            this.inputHandler.changeCursorStyle("pointer")
+                            this.inputHandler.canvas.addEventListener("mousedown", mouseDownHandler)
+                        }
+                    }
                     break
                 default:
                     console.warn("REQUISITO NÃO SUPORTADO")
