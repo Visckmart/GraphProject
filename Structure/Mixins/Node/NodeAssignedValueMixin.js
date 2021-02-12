4fca0b0227cded6820a87b812fbee94aad34b2f6
Visckmart <visckmart@hotmail.com>
2021-02-12 20:52:57 -0300

    Organização da serialização e da label dos nós.

--- Structure/Mixins/Node/NodeAssignedValueMixin.js
+++ Structure/Mixins/Node/NodeAssignedValueMixin.js
@@ -34,46 +34,44 @@ let NodeAssignedValueMixin = (superclass) => {
         }
 
         drawAuxLabel = () => {
-            if(!this.assignedValue)
-            {
-                return
-            }
-            ctx.save()
-            ctx.font = "bold 15pt Arial";
+            if(!this.assignedValue || this.assignedValue == "") { return; }
+
+            ctx.save();
+
             if (this.assignedValue == "∞") {
                 ctx.font = "bold 20pt Arial";
+            } else {
+                ctx.font = "bold 15pt Arial";
             }
-            ctx.strokeStyle = this._originalcolor;
-            ctx.setLineDash([])
-            ctx.lineWidth = 4
             ctx.textAlign = "center";
             ctx.textBaseline = 'middle';
-            let m = ctx.measureText(this.assignedValue)
-            let boxWidth = m.width + 5
-            let boxHeight = 20
-            let boxOffset = 45
+
+            let boxWidth = ctx.measureText(this.assignedValue).width + 5;
+            let boxHeight = 20;
+            let boxVerticalOffset = 45;
+
             ctx.beginPath();
             roundRect(ctx,
                 this.pos.x - boxWidth/2,
-                this.pos.y - boxOffset - boxHeight/2,
-                boxWidth, boxHeight, 5)
-            ctx.fillStyle = this._originalcolor
-            if (this.assignedValue != "∞") {
-            ctx.fill();
-        }
-            ctx.fillStyle = "#888"
-            if (this.assignedValue != "∞") {
-            ctx.fillStyle = "white"
-        }
-            // ctx.stroke()
-            // ctx.fillStyle = this._originalcolor
-            ctx.fillText(this.assignedValue, this.pos.x, this.pos.y - boxOffset);
-            
-            ctx.fillStyle = "white"
+                this.pos.y - boxHeight/2 - boxVerticalOffset,
+                boxWidth, boxHeight, 5);
+
             if (this.assignedValue == "∞") {
-                ctx.fillStyle = colorFromComponents(180, 180, 180, 0.5)
+                ctx.fillStyle = colorFromComponents(180, 180, 180, 0.5);
                 ctx.fill();
+
+                // Text color
+                ctx.fillStyle = "#888";
+            } else {
+                ctx.fillStyle = this._originalcolor;
+                ctx.fill();
+
+                // Text color
+                ctx.fillStyle = "white";
             }
+            ctx.fillText(this.assignedValue,
+                         this.pos.x, this.pos.y - boxVerticalOffset);
+
             ctx.restore()
             return 0;
         }
--- Structure/NodeSerialization.js
+++ Structure/NodeSerialization.js
@@ -3,20 +3,39 @@ import {HighlightsHandler} from "./Highlights.js";
 import {positionAlphabet} from "./Utilities.js";
 import Node from "./Node.js"
 
+
+function encodePosition(relativeX, relativeY, alphabet = positionAlphabet) {
+    const alphabetSize = alphabet.length - 1;
+    let indexX = Math.round(relativeX * alphabetSize);
+    let indexY = Math.round(relativeY * alphabetSize);
+    return [alphabet[indexX], alphabet[indexY]];
+}
+function decodePosition(serializedPos, alphabet = positionAlphabet) {
+    const alphabetSize = alphabet.length - 1;
+    let indexX = alphabet.indexOf(serializedPos.charAt(0));
+    let indexY = alphabet.indexOf(serializedPos.charAt(1));
+    if (indexX == null || indexY == null) return;
+    let relativeX = indexX/alphabetSize;
+    let relativeY = indexY/alphabetSize;
+    return [relativeX, relativeY];
+}
 export function serializeNode() {
     // Colors
+    // Se for escolhida automaticamente, serialize o índice,
+    // caso contrário serialize o hexadecimal da cor.
     let serializedColors;
-    let indexOfColor = nodeColorList.indexOf(this._originalcolor)
-    if (indexOfColor) {
-        serializedColors = this._originalcolor.slice(1)
-    } else {
+    let indexOfColor = nodeColorList.indexOf(this._originalcolor);
+    if (indexOfColor >= 0) {
         serializedColors = indexOfColor;
+    } else {
+        serializedColors = this._originalcolor.slice(1);
     }
 
     // Position
-    let percX = Math.round((this.pos.x / canvas.width)*61);
-    let percY = Math.round((this.pos.y / canvas.height)*61);
-    let serializedPosition = `${positionAlphabet[percX]}${positionAlphabet[percY]}`
+    // Transforma a posição absoluta em uma posição relativa ao tamanho do
+    // canvas e converte para um caractere do alfabeto escolhido para isso.
+    let [encodedX, encodedY] = encodePosition(this.pos.x / canvas.width, this.pos.y / canvas.height);
+    let serializedPosition = `${encodedX}${encodedY}`
 
     // Highlights
     let serializedHighlights = this.highlights.prepareForSharing()
@@ -30,37 +49,34 @@ export function serializeNode() {
 export function deserializeNode(serializedNode) {
     const nodeSerializationFormat = /(\d+)-(.+?)-(.+)?/i;
     let matchResult = serializedNode.match(nodeSerializationFormat);
-    // console.log(serializedNode, matchResult)
     if (!matchResult) return;
+    // Basic
     let [, index, serializedColor, moreInfo] = matchResult;
     index = parseInt(index)
-    // console.log("index", index)
 
+    // Color
     let colorMatchResult = serializedColor.match(/([a-fA-F0-9]{6})|(\d+)/i);
     if (!colorMatchResult) return;
     let [, customColor, colorIndex] = colorMatchResult;
     let color = customColor ?? nodeColorList[colorIndex % nodeColorList.length];
-    // console.log("color", color)
 
+    // Label and position
     const moreInfoFormat = /((?<label>[a-zA-Z0-9]{1,2})(?<pos>[a-zA-Z0-9]{2}))(-(?<highlights>.+))?/i
     let moreInfoResult = moreInfo.match(moreInfoFormat);
     if (!moreInfoResult) return;
-
     let label = moreInfoResult.groups.label
     let serializedPos = moreInfoResult.groups.pos
     let serializedHighlights = moreInfoResult.groups.highlights
-    // console.log("label", label)
-    // console.log("pos", serializedPos)
+
+    // Highlights
     let highlights;
     if (serializedHighlights != null) {
         highlights = HighlightsHandler.deserialize(serializedHighlights)
     }
 
-    let xPos = positionAlphabet.indexOf(serializedPos.charAt(0))
-    let yPos = positionAlphabet.indexOf(serializedPos.charAt(1))
-    if (xPos == null || yPos == null) return;
-    xPos *= canvas.width/61;
-    yPos *= canvas.height/61;
+    let [relativeX, relativeY] = decodePosition(serializedPos);
+    let xPos = relativeX * canvas.width;
+    let yPos = relativeY * canvas.height;
 
     return new Node({
                         x: xPos, y: yPos, label, index, color, highlights
