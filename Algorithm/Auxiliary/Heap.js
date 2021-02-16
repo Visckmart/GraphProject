5d8879405c3a99650ec90575bcbc96846934cde5
Thlamz <thlamenza@hotmail.com>
2021-02-15 22:25:19 -0300

    Implementando desenho do heap

--- Algorithm/Auxiliary/Heap.js
+++ Algorithm/Auxiliary/Heap.js
@@ -1,11 +1,25 @@
-class Heap {
+import AlgorithmShowcase from "../../Drawing/AlgorithmControls/AlgorithmShowcase.js";
+
+const elementRadius = 15
+const distanceBetweenLevels = 50
+const distanceBetweenElements = 38
+const paddingTop = 50
+
+class Heap extends AlgorithmShowcase{
     constructor() {
+        super()
+
+
         this._heap = []
         this._heapSize = 0
         // Map para encontrar elementos em O(n)
         this._elementMap = new Map()
+
+        this._steps = []
+        this._messages = []
     }
 
+    //#region Funções do heap
     insert(element, value) {
         this._heap[this._heapSize] = {element, value}
         this._heapSize++
@@ -13,6 +27,7 @@ class Heap {
         this._elementMap.set(element, this._heapSize - 1)
 
         this._siftUp(this._heapSize - 1)
+        this._messages.push(`Inserindo o elemento ${element.toString()} com valor ${value} no heap e ajustando para cima.`)
     }
 
     remove() {
@@ -40,6 +55,7 @@ class Heap {
         }
 
         this._elementMap.delete(returnValue)
+        this._messages.push(`Removendo o elemento ${returnValue.toString()} no heap e ajustando para baixo.`)
         return returnValue
     }
 
@@ -58,11 +74,15 @@ class Heap {
         if(this._heap[index]?.element === element)
         {
             let isNewValueBetter = this._compare(this._heap[index].value, value)
+
+            let oldValue = this._heap[index].value
             this._heap[index].value = value
 
             if(isNewValueBetter) {
+                this._messages.push(`Mudando o valor do elemento ${element.toString()} de ${oldValue} para ${value} no heap e ajustando para cima.`)
                 this._siftUp(index)
             } else {
+                this._messages.push(`Mudando o valor do elemento ${element.toString()} de ${oldValue} para ${value} no heap e ajustando para baixo.`)
                 this._siftDown(index)
             }
         }
@@ -118,12 +138,130 @@ class Heap {
         this._elementMap.set(this._heap[index1].element, index1)
         this._elementMap.set(this._heap[index2].element, index2)
     }
+    //#endregion
+    //#region Funções de showcase
+    addStep() {
+        let fullMessage = this._messages.length > 0 ? 'Ações dessa etapa:\n' : 'Nenhuma ação feita nessa etapa'
+        for(let message of this._messages) {
+            fullMessage += '• ' + message + '\n\n'
+        }
 
+        this._steps.push({
+            heap: Array.from(this._heap),
+            heapSize: this._heapSize,
+            message: fullMessage
+        })
+        this._messages = []
+    }
+
+    loadStep(number) {
+        if(this._steps[number]) {
+            this._heapSize = this._steps[number].heapSize
+            this._heap = this._steps[number].heap
+
+            this.showcaseMessage = this._steps[number].message
+
+            this.resizeCanvas(this.body.clientWidth,
+                Math.log2(this._heapSize) * (elementRadius + distanceBetweenLevels) + paddingTop)
+
+            requestAnimationFrame(this.drawHeap)
+        }
+    }
+
+    drawHeap = () => {
+        if(this._heapSize > 0)
+        {
+            let ctx = this.ctx
+            ctx.save()
+            ctx.beginPath()
+            ctx.textAlign = 'center'
+            ctx.textBaseline = 'top'
+            ctx.fillStyle = '#ffffff'
+            ctx.font = '20px Arial'
+            ctx.fillText('Estado atual do heap:', this.canvas.width/2, 0)
+            ctx.restore()
+            this.drawHeapElement(this._heap[0].element, 0, this.canvas.width/2, paddingTop, false)
+        }
+    }
+
+    _drawElement(x, y, element) {
+        let ctx = this.ctx
+        ctx.save()
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#8b0000'
+        ctx.fillStyle = '#ff726f'
+
+        ctx.beginPath();
+        ctx.arc(x, y, elementRadius, 0, 2*Math.PI);
+        ctx.fill()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.textAlign = 'center'
+        ctx.textBaseline = 'middle'
+        ctx.font = 'bold 15px Arial'
+        ctx.fillStyle = '#ffffff'
+        ctx.fillText(element.toString(), x, y, elementRadius * 2)
+        ctx.restore()
+    }
+
+    _startDrawLines = (x, y) => {
+        let ctx = this.ctx
+        ctx.save()
+        ctx.lineWidth = 2
+        ctx.strokeStyle = "#fff";
+        ctx.setLineDash([]);
+
+        ctx.beginPath()
+        ctx.moveTo(x, y);
+    }
+
+    _finishDrawLines = (x, y) => {
+        let ctx = this.ctx
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.restore()
+    }
+
+    drawHeapElement = (element, index, parentX, parentY, isLeft) => {
+        let x, y
+        // Caso seja o primeiro elemento desenha no centro
+        if(index === 0) {
+            x = parentX
+            y = parentY
+        // Caso contrário desenha do lado especificado pelo parâmetro
+        } else {
+            let levelDistance = (Math.floor(Math.log2(this._heapSize)) - Math.floor(Math.log2(index + 1) - 1)) * distanceBetweenElements
+            x = parentX + (isLeft ?
+                -levelDistance/2 :
+                levelDistance/2)
+            y = parentY + distanceBetweenLevels
+            this._finishDrawLines(x, y)
+        }
+
+
+        // Se existe filho a esquerda chama a recursão a esquerda
+        let leftChildIndex = index * 2 + 1
+        if(this._heap[leftChildIndex]) {
+            this._startDrawLines(x, y)
+            this.drawHeapElement(this._heap[leftChildIndex].element, leftChildIndex, x, y, true)
+        }
+
+        // Se existe filho a direita chama a recursão a direita
+        let rightChildIndex = leftChildIndex + 1
+        if(this._heap[rightChildIndex]) {
+            this._startDrawLines(x, y, false)
+            this.drawHeapElement(this._heap[rightChildIndex].element, rightChildIndex, x, y, false)
+        }
+        this._drawElement(x,y, element)
+    }
+    //#endregion
 }
 
 export class MaxHeap extends Heap {
     constructor() {
         super();
+        this.showcaseTitle = 'Max Heap Binário'
     }
 
     _compare(value1, value2) {
@@ -134,6 +272,8 @@ export class MaxHeap extends Heap {
 export class MinHeap extends Heap {
     constructor() {
         super();
+
+        this.showcaseTitle = 'Min Heap Binário'
     }
 
     _compare(value1, value2) {
