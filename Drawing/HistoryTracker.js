cbb20cc11d5d79e5f2c8ce1d797de9e82c791399
Visckmart <visckmart@hotmail.com>
2021-02-19 01:42:53 -0300

    Documentando e consertando um problema no histórico.

--- Drawing/HistoryTracker.js
+++ Drawing/HistoryTracker.js
@@ -1,18 +1,28 @@
 export default class HistoryTracker {
+    // Lista de grafos com cada passo relevante
     steps = [];
+    // Marcador do passo atual
     cursor = -1;
 
+    // Avança o marcador e armazena um novo passo
     registerStep(newStep) {
+        // Anda com o marcador
         this.cursor += 1;
-        this.steps.splice(this.cursor)
+        // Apaga todos os passos posteriores
+        this.steps.splice(this.cursor);
+        // Armazena o passo atual
         this.steps[this.cursor] = newStep.clone();
-        // console.log("Registered step", this.cursor)
     }
 
+    // Move o marcador como solicitado e retorna o conteúdo do passo
     goToStep(relativePosition) {
+        // Anda com o marcador
         this.cursor += relativePosition;
-        this.cursor = Math.min(Math.max(this.cursor, 0), this.steps.length-1)
-        // console.log("Going to step", this.cursor)
+        // Não permite o marcador sair dos limites
+        let cursorEnd = this.steps.length-1;
+        this.cursor = Math.min(Math.max(this.cursor, 0), cursorEnd)
+
+        // Retorna o passo solicitado
         return this.steps[this.cursor].clone();
     }
 }
