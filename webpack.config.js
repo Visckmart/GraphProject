f35ac0eacd8c764c8b9f00a0eb39afe34601b360
Thlamz <thlamenza@hotmail.com>
2021-08-08 14:14:37 -0300

    Evoluindo setup do webpack com babel

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,19 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: [
+                            ["@babel/transform-runtime"]
+                        ]
+                    }
+                }
+            },
+            {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
